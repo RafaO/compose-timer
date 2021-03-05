cc8df29b0ed7ea3812db6e6ab0fca25ab6074df6
Rafael Ortega <rafao1989@hotmail.com>
2021-03-05 21:32:01 +0100

    display fixed countdown with bar animation

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,12 +18,31 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,8 +58,39 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
+    val scope = rememberCoroutineScope()
+    var time by remember { mutableStateOf(300) }
+    val transition = updateTransition(time)
+    val indicatorHeight by transition.animateDp { it.dp }
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        Row(modifier = Modifier.height(500.dp), verticalAlignment = Alignment.Bottom) {
+            Column {
+                Text(text = "$time secs")
+                Button(
+                    onClick = {
+                        scope.launch {
+                            while (time > 0) {
+                                delay(100)
+                                time -= 1
+                            }
+                        }
+                    }
+                ) {
+                    Text("start")
+                }
+                Button(
+                    onClick = {
+                        time = 300
+                    }
+                ) { Text("reset") }
+            }
+            Column(
+                Modifier
+                    .height(indicatorHeight)
+                    .width(30.dp)
+                    .background(Color.Cyan)
+            ) {}
+        }
     }
 }
 
